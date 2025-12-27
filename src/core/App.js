cde3a9e568108b648e25ddfefc78d36e3c5e0273
Maja Glowania <maiaglowania13@gmail.com>
2025-12-27 18:15:44 +0100

    Creat React app, styled-components and clean up

---
+++ src/core/App.js
@@ -0,0 +1,12 @@
+import Container from "../common/Container";
+import Header from "../features/homepage/Header";
+
+function App() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/core/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  color: {
+    background: "#FBFBFE",
+    textPrimary: "#252525",
+    textSecondary: "#6E7E91",
+    accent: "#0366D6",
+    white: "#FFFFFF",
+    iron: "rgba(209, 213, 219, 0.3)",
+  },
+  breakpoint: {
+    mobile: 767,
+  },
+};
