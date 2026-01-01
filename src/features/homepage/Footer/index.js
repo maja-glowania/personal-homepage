6c8c1472990a88133b3234e0f25d93136f5d2a7d
Maja Glowania <maiaglowania13@gmail.com>
2026-01-01 19:06:41 +0100

    refactor: use map to render social links and remove LinkedIn

--- src/features/homepage/Footer/index.js
+++ src/features/homepage/Footer/index.js
@@ -7,52 +7,31 @@ import {
   SocialList,
   SocialLink,
 } from "./styled";
-import { GithubIcon } from "../../../common/icons/GithubIcon";
-import { LinkedInIcon } from "../../../common/icons/LinkedInIcon";
-import { FacebookIcon } from "../../../common/icons/FacebookIcon";
-import { InstagramIcon } from "../../../common/icons/InstagramIcon";
+import { socials } from "./socialsData";
+import { email } from "../../../common/email";
 
 const Footer = () => (
   <Wrapper>
     <LetsTalk>Let's talk!</LetsTalk>
     <Address>
-      <Email href="mailto:glowania.maja02@gmail.com">
-        glowania.maja02@gmail.com
-      </Email>
+      <Email href={`mailto:${email}`}>{email}</Email>
       <Paragraph>
         I’m always open to new projects whenever I have the time. If you have a
         website, dashboard or mobile app in mind and need some help to make your
         ideas come to life, feel free to contact me🤞🏻
       </Paragraph>
       <SocialList>
-        <SocialLink
-          href="https://github.com/maja-glowania"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <GithubIcon />
-        </SocialLink>
-        <SocialLink
-          href="https://www.facebook.com/profile.php?id=100005432553897"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FacebookIcon />
-        </SocialLink>
-        <SocialLink
-          href="https://pl.linkedin.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <LinkedInIcon />
-        </SocialLink>
-        <SocialLink
-          href="https://www.instagram.com/maiaglowania/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <InstagramIcon />
-        </SocialLink>
+        {socials.map(({ name, url, Icon }) => (
+          <SocialLink
+            key={name}
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            title={name}
+          >
+            <Icon />
+          </SocialLink>
+        ))}
       </SocialList>
     </Address>
   </Wrapper>
---
+++ src/features/homepage/Footer/socialsData.js
@@ -0,0 +1,21 @@
+import { GithubIcon } from "../../../common/icons/GithubIcon";
+import { FacebookIcon } from "../../../common/icons/FacebookIcon";
+import { InstagramIcon } from "../../../common/icons/InstagramIcon";
+
+export const socials = [
+  {
+    name: "GitHub",
+    url: "https://github.com/maja-glowania",
+    Icon: GithubIcon,
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/profile.php?id=100005432553897",
+    Icon: FacebookIcon,
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/maiaglowania/",
+    Icon: InstagramIcon,
+  },
+];
